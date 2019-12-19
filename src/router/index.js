0c7728a386ce16e8cd02c3d2ed3df067db212743
Mr Li <1073146616@qq.com>
2019-12-19 20:42:26 +0800

    save

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,42 @@ import Vue from 'vue'
 import Router from 'vue-router'
 //引入Home页面
 import Home from "../components/Home/Home"
-Vue.use(Router)
+//引入person我的页面
+import Person from "../components/person/Person"
+//引入info页面
+import Info from "../components/person/info"
+//引入setsex页面
+import Setsex from "../components/person/setsex"
+//引入class页面
+import Class from "../components/person/class"
+
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect:"/person"
+    },
+    {
+      path:"/person",
+      name:"Person",
+      component:Person
+    },
+    {
+      path:"/info",
+      name:"info",
+      component: Info
+    },
+    {
+      path:"/setsex",
+      name:"setsex",
+      component:Setsex
+    },
+    {
+      path:"/class",
+      name:"class",
+      component:Class
     }
   ]
 })
