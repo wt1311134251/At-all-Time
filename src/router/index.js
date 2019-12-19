2dee8e333f2b1bc2756edb932c87c368f4e9a0da
ZhongLinFeng886 <58970593+ZhongLinFeng886@users.noreply.github.com>
2019-12-19 11:15:07 +0800

    -mhh

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 //引入Home页面
 import Home from "../components/Home/Home"
-
-
 Vue.use(Router)
 
 export default new Router({
