2e945c454fd98e9e9e4e22a535bc7440e951eb87
dingding <929763179@qq.com>
2019-12-19 20:51:34 +0800

    dingding

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 //引入Home页面
 import Home from "../components/Home/Home"
 
 
+import Record from '../components/Record/record'
+import Oto from '../components/Record/oto'
+import Search from '../components/Record/Search'
+import Otop from '../components/Record/oto-plan'
 Vue.use(Router)
 
 export default new Router({
@@ -12,7 +15,22 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component:Record
+    },
+    {
+      path:'/oto',
+      name:'oto',
+      component:Oto
+    },
+    {
+      path:'/search',
+      name:'search',
+      component:Search
+    },
+    {
+      path:'/oto-plan',
+      name:'oto-plan',
+      component:Otop
     }
   ]
 })
