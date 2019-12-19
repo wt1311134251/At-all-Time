53ba9bf3f9b645a7c062f9f710adbc46519cf613
ZhongLinFeng886 <58970593+ZhongLinFeng886@users.noreply.github.com>
2019-12-19 10:00:30 +0800

    create

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+ //引入 vue
+ import  Vue from "vue"
+
+ //引入vuex
+ import vuex from "vuex"
+
+ //挂载
+ Vue.use(vuex)
+
+ //创建store
+ export default new vuex.Store({
+        state:{
+          title:"msmk"
+        }
+ })
