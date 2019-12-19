edc72371afe8dc67393c486a3d9d41c501eeb933
Mr Li <1073146616@qq.com>
2019-12-19 20:46:08 +0800

    two

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,9 @@
-import Vue from 'vue'
+﻿import Vue from 'vue'
 import Router from 'vue-router'
 // 引入Home页面
 import Home from "../components/Home/Home"
 <<<<<<< HEAD
-//引入person我的页面
-import Person from "../components/person/Person"
-//引入info页面
-import Info from "../components/person/info"
-//引入setsex页面
-import Setsex from "../components/person/setsex"
-//引入class页面
-import Class from "../components/person/class"
+
 
 Vue.use(Router);
 =======
